96e0a3d3fa52ebbd4337d171d8f1a81f103955be
GedSargent <120935232+GedSargent@users.noreply.github.com>
2026-03-19 09:13:21 +0000

    Remove comments no longer needed

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,6 @@ import { Checkbox } from '@/components/ui/checkbox';
 export default function HomeScreen() {
   return (
     <View className="bg-background flex flex-1">
-      {/* Task starts here */}
       <View className="flex flex-row">
         <View className="flex w-16 items-center justify-center">
           <Checkbox checked={true} className="border-2" />
@@ -15,7 +14,6 @@ export default function HomeScreen() {
           <Text className="text-foreground-transparent">Due: Oct 20</Text>
         </View>
       </View>
-      {/* Task ends here */}
     </View>
   );
 }
