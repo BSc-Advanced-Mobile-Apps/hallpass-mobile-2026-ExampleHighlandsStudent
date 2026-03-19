bb66155ad2a575c7b36af91bcd8a629840b94155
GedSargent <120935232+GedSargent@users.noreply.github.com>
2026-03-19 12:09:33 +0000

    IN PROGRESS - Started to extract Task component

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,29 @@
+import * as React from 'react';
 import { View } from 'react-native';
 import { Text } from '@/components/ui/text';
 import { Checkbox } from '@/components/ui/checkbox';
 
+export interface ITask {
+  title: string;
+  category: string;
+  isChecked: boolean;
+}
+
 export default function HomeScreen() {
+  // 1. Define your useState variables here
+  const [checked, setChecked] = React.useState(false);
+
+  const task: ITask = {
+    title: 'My test item',
+    category: 'Test category',
+    isChecked: false,
+  };
+
   return (
     <View className="bg-background flex flex-1">
-      <View className="flex flex-row">
-        <View className="flex w-16 items-center justify-center">
-          <Checkbox checked={false} className="border-2" />
-        </View>
-        <View className="border-foreground-transparent w-full border-b py-4">
-          <Text className="text-foreground">Submit Assignment</Text>
-          <Text className="text-foreground-transparent">Biology</Text>
-        </View>
-      </View>
+      {/* Start of single task code */}
+
+      {/* End of single task code */}
     </View>
   );
 }
